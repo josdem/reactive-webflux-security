584f582567057e6223d2afa7caec53fd7241fd25
josdem <joseluis.delacruz@gmail.com>
2018-04-08 15:52:38 -0500

    Adding security controller class

---
+++ src/main/java/com/jos/dem/security/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.jos.dem.security.EnableWebFluxSecurity;
+
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    http
+      .authorizeExchange()
+      .anyExchange().authenticated()
+      .and()
+      .httpBasic().and()
+      .formLogin();
+    return http.build();
+  }
+
+  @Bean
+  public MapReactiveUserDetailsService userDetailsService() {
+    UserDetails user = User.withDefaultPasswordEncoder()
+      .username("josdem")
+      .password("12345678")
+      .roles("USER")
+      .build();
+    return new MapReactiveUserDetailsService(user);
+  }
+
+}
