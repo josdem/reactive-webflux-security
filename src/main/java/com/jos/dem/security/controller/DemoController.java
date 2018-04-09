5d5a4382622602c7f418ee4bfa5c5ec440f5e990
josdem <joseluis.delacruz@gmail.com>
2018-04-09 15:53:54 -0500

    Adding home template

--- src/main/java/com/jos/dem/security/controller/DemoController.java
+++ src/main/java/com/jos/dem/security/controller/DemoController.java
@@ -1,7 +1,9 @@
 package com.jos.dem.security.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class DemoController {
 
   @GetMapping("/")
