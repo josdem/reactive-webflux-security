f0a027500d873d55e9e1074beaf07eeb87a09a96
josdem <joseluis.delacruz@gmail.com>
2018-04-09 20:56:04 -0500

    Getting name from principal

--- src/main/java/com/jos/dem/security/controller/DemoController.java
+++ src/main/java/com/jos/dem/security/controller/DemoController.java
@@ -3,6 +3,8 @@ package com.jos.dem.security.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,8 +14,8 @@ public class DemoController {
   private Logger log = LoggerFactory.getLogger(this.getClass());
 
   @GetMapping("/")
-  public String index() {
-    log.info("Calling index");
+  public String index(Principal principal) {
+    log.info("Hello, " + principal.getName() + "!");
     return "home";
   }
 
