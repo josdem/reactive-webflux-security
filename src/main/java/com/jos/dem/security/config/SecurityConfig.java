8c78d5de382269b3fbfef8332ed900297f40b5a1
josdem <joseluis.delacruz@gmail.com>
2018-04-11 10:45:40 -0500

    Refactoring to make it simple

--- src/main/java/com/jos/dem/security/config/SecurityConfig.java
+++ src/main/java/com/jos/dem/security/config/SecurityConfig.java
@@ -2,12 +2,11 @@ package com.jos.dem.security.config;
 
 import com.jos.dem.security.repository.UserRepository;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
@@ -30,8 +29,8 @@ public class SecurityConfig {
   }
 
   @Bean
-  public ReactiveUserDetailsService userDetailsService() {
-    return (username) -> userRepository.findByUsername(username).cast(UserDetails.class);
+  public ReactiveUserDetailsService  userDetailsService() {
+    return (username) -> userRepository.findByUsername(username);
   }
 
 }
--- src/main/java/com/jos/dem/security/repository/UserRepository.java
+++ src/main/java/com/jos/dem/security/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.jos.dem.security.repository;
 
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Mono;
-
 import com.jos.dem.security.model.User;
 
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import reactor.core.publisher.Mono;
+
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
-  Mono<User> findByUsername(String username);
+  Mono<UserDetails> findByUsername(String username);
 }
