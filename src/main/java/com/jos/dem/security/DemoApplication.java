f99ba0838a70aae63b6ec4a96c4c9c5a90c76206
josdem <joseluis.delacruz@gmail.com>
2018-04-11 12:04:03 -0500

    Getting user stored in the database

--- src/main/java/com/jos/dem/security/DemoApplication.java
+++ src/main/java/com/jos/dem/security/DemoApplication.java
@@ -1,12 +1,42 @@
 package com.jos.dem.security;
 
+import java.util.UUID;
+
+import com.jos.dem.security.repository.UserRepository;
+import com.jos.dem.security.model.User;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return NoOpPasswordEncoder.getInstance();
+  }
+  
+  @Bean
+  CommandLineRunner start(UserRepository userRepository, PasswordEncoder passwordEncoder){
+    return args -> {
+      userRepository.deleteAll().subscribe();
+      
+      User user = new User();
+      user.setUuid(UUID.randomUUID().toString());
+      user.setUsername("josdem");
+      user.setPassword(passwordEncoder.encode("12345678"));
+      userRepository.save(user).subscribe();      
+
+      userRepository.findAll().log().subscribe(System.out::println);
+    };
+  }
+  
 }
