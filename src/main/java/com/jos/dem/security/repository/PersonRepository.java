18fc9041b35d8d461f5e72fba93d04814bcb1b89
josdem <joseluis.delacruz@gmail.com>
2019-06-08 14:14:44 -0400

    Adding user repository

--- src/main/java/com/jos/dem/security/repository/PersonRepository.java
+++ src/main/java/com/jos/dem/security/repository/PersonRepository.java
@@ -1,7 +1,12 @@
 package com.jos.dem.security.repository;
 
+import com.jos.dem.security.model.User;
+
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import com.jos.dem.webflux.model.Person;
+import reactor.core.publisher.Mono;
 
-public interface PersonRepository extends ReactiveMongoRepository<Person, String> {}
+public interface UserRepository extends ReactiveMongoRepository<User, String> {
+  Mono<UserDetails> findByUsername(String username);
+}
