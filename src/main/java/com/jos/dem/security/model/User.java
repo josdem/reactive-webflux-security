d0d9028e9f55b5110da50ac96bcf63cf0e428641
josdem <joseluis.delacruz@gmail.com>
2019-06-08 14:13:30 -0400

    Adding user model

---
+++ src/main/java/com/jos/dem/security/model/User.java
@@ -0,0 +1,73 @@
+package com.jos.dem.security.model;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@Document
+@ToString
+public class User implements UserDetails {
+
+  @Id
+  private String uuid;
+  private String username;
+  private String password;
+
+  private boolean active = true;
+  private Set<GrantedAuthority> roles = new HashSet<GrantedAuthority>();
+
+  @Builder
+  public User(String uuid, String username, String password){
+    this.uuid = uuid;
+    this.username = username;
+    this.password = password;
+    roles.add(new SimpleGrantedAuthority("ROLE_USER"));
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return roles;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return active;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return active;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return active;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return active;
+  }
+
+  @Override
+  public String getPassword() {
+    return this.password;
+  }
+
+  @Override
+  public String getUsername() {
+    return this.username;
+  }
+
+}
