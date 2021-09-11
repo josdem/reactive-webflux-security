1b1d059caebdce72e9c3058e8a8c1d8130e01766
josdem <joseluis.delacruz@gmail.com>
2021-09-11 13:52:01 -0500

    #2 Refactoring test name

--- src/test/java/com/jos/dem/security/SecurityControllerTest.java
+++ src/test/java/com/jos/dem/security/SecurityControllerTest.java
@@ -1,6 +1,7 @@
 package com.jos.dem.security;
 
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +14,8 @@ class SecurityControllerTest {
   private final WebTestClient webTestClient;
 
   @Test
-  public void contextLoads() {
+  @DisplayName("User is unauthorized")
+  void shouldValidateUnauthorized() {
     webTestClient.get().uri("/").exchange().expectStatus().isUnauthorized();
   }
 }
