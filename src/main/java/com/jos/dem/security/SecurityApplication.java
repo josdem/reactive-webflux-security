a623c79fd1d1537ead5646e618ba25e6edc78cab
josdem <joseluis.delacruz@gmail.com>
2021-09-12 09:08:40 -0500

    #3 Adding Lombok annotations

--- src/main/java/com/jos/dem/security/SecurityApplication.java
+++ src/main/java/com/jos/dem/security/SecurityApplication.java
@@ -2,8 +2,7 @@ package com.jos.dem.security;
 
 import com.jos.dem.security.model.User;
 import com.jos.dem.security.repository.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,10 +10,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Slf4j
 @SpringBootApplication
 public class SecurityApplication {
-
-  private Logger log = LoggerFactory.getLogger(this.getClass());
 
   public static void main(String[] args) {
     SpringApplication.run(SecurityApplication.class, args);
