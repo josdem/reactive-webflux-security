08c15b653f3bf602d069e274c5da0c8670c3d594
josdem <joseluis.delacruz@gmail.com>
2021-09-12 08:09:35 -0500

    Adding gradle generated files to gitignore

--- src/main/java/com/jos/dem/security/model/User.java
+++ src/main/java/com/jos/dem/security/model/User.java
@@ -1,33 +1,31 @@
 package com.jos.dem.security.model;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.ToString;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Document
 @ToString
 public class User implements UserDetails {
 
-  @Id
-  private String username;
+  @Id private String username;
   private String password;
 
   private boolean active = true;
   private Set<GrantedAuthority> roles = new HashSet<GrantedAuthority>();
 
   @Builder
-  public User(String username, String password){
+  public User(String username, String password) {
     this.username = username;
     this.password = password;
     roles.add(new SimpleGrantedAuthority("ROLE_USER"));
@@ -67,5 +65,4 @@ public class User implements UserDetails {
   public String getUsername() {
     return this.username;
   }
-
 }
